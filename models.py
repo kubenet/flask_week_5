cf129cb8f81881cab1f3bba9d5baf6ff911a23af
kubenet <kubenet@gmail.com>
2020-08-19 02:37:05 +0700

    add views.py and models.py

--- models.py
+++ models.py
@@ -1,67 +1,42 @@
 from app import db
 
 
-class Teachers(db.Model):
-    """ Модель преподавателей """
-    __tablename__ = 'teachers'
+class Persons(db.Model):
+    """ Модель пользователь """
+    __tablename__ = 'persons'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, nullable=False)
-    about = db.Column(db.String, nullable=False)
-    rating = db.Column(db.Float, nullable=False)
+    mail = db.Column(db.String, nullable=False)
+    password = db.Column(db.String, nullable=False)
+    orders = db.relationship("Goals", back_populates="goal")
+
+
+class Meals(db.Model):
+    """ Модель блюдо """
+    __tablename__ = 'meals'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    price = db.Column(db.String, nullable=False)
+    description = db.Column(db.Float, nullable=False)
     picture = db.Column(db.String, nullable=False)
-    price = db.Column(db.Integer, nullable=False)
-    lesson_time = db.Column(db.String)
-    # Ссылка на поле в модели цели (One-to-Many)
-    goals = db.relationship("Goals", back_populates="goal")
-    # Ссылка на поле в модели рассписание (One-to-Many)
-    week_day = db.relationship("TimetableTeachers", back_populates="week")
+    category = db.relationship("Categories", back_populates="title")
 
 
-class Goals(db.Model):
-    """ Модель целей занятий """
-    __tablename__ = 'goals'
+class Categories(db.Model):
+    """ Модель категории """
+    __tablename__ = 'categories'
     id = db.Column(db.Integer, primary_key=True)
-    key = db.Column(db.String, nullable=False)
-    # Ссылка на модель преподавателя (One-to-Many)
-    teachers_id = db.Column(db.Integer, db.ForeignKey("teachers.id"))
-    goal = db.relationship("Teachers", back_populates="goals", uselist=False)
-
-    # Ссылка на поле в модели подбора преподавателя (One-to-Many)
-    search_teacher = db.relationship("SearchTeacher", back_populates="goal", uselist=False)
+    title = db.Column(db.String, nullable=False)
+    goal = db.relationship("Meals", back_populates="goals", uselist=False)
 
 
-class TimetableTeachers(db.Model):
-    """ Модель расписания преподавателей на неделю """
-    __tablename__ = 'timetables'
+class Orders(db.Model):
+    """ Модель заказы """
+    __tablename__ = 'orders'
     id = db.Column(db.Integer, primary_key=True)
-    day_times = db.Column(db.String, nullable=False)
+    date = db.Column(db.String, nullable=False)
+    sum = db.Column(db.Boolean, nullable=False)
     status = db.Column(db.Boolean, nullable=False)
-    # ссылка на поле id в модели преподавателя (One-to-Many)
-    teachers_id = db.Column(db.Integer, db.ForeignKey("teachers.id"))
-    week = db.relationship("Teachers", back_populates="week_day", uselist=False)
-    # ссылка на поле id в модели booking (One-to-Many)
-    booking = db.relationship("Booking", back_populates='day_times', uselist=False)
-
-
-class SearchTeacher(db.Model):
-    """ Модель поиска преподавателя по критериям: цели и планируемое кол-во часов занятий в неделю """
-    __tablename__ = 'search_teachers'
-    id = db.Column(db.Integer, primary_key=True)
-    how_time = db.Column(db.String(20), nullable=False)
-    client_name = db.Column(db.String(25), nullable=False)
-    client_phone = db.Column(db.String(10), nullable=False)
-    # Ссылка на поле в модели цели (One-to-Many)
-    goal_id = db.Column(db.Integer, db.ForeignKey("goals.id"))
-    goal = db.relationship("Goals", back_populates="search_teacher", uselist=False)
-
-
-class Booking(db.Model):
-    """ Модель для записи на пробное занятие к преподавателю """
-    __tablename__ = 'booking'
-    id = db.Column(db.Integer, primary_key=True)
-    client_name = db.Column(db.String(25), nullable=False)
-    client_phone = db.Column(db.String(10), nullable=False)
-    # ссылка на поле id в модели Teachers (One-to-Many)
-    timetable_id = db.Column(db.Integer, db.ForeignKey("timetables.id"))
-    # ссылка на поле free и day в модели TimetableTeachers (One-to-One)
-    day_times = db.relationship("TimetableTeachers", back_populates="booking", uselist=False)
+    email = db.Column(db.String, nullable=False)
+    phone = db.Column(db.String, nullable=False)
+    address = db.Column(db.String, nullable=False)
+    list_meals = db.Column(db.String, nullable=False)
--- views.py
+++ views.py
@@ -3,7 +3,7 @@ from app import app, render_template, request
 
 @app.route('/')  # главная
 def index():
-    return render_template("index.html")
+    return render_template("main.html")
 
 
 @app.route('/cart/')  # для корзины
@@ -16,6 +16,11 @@ def account():
     return render_template("account.html")
 
 
+@app.route('/login/')  # для аутентификации
+def login():
+    return render_template("login.html")
+
+
 @app.route('/register/')  # для регистрации
 def register():
     return render_template("register.html")
@@ -23,8 +28,7 @@ def register():
 
 @app.route('/logout/')  # для аутентификации
 def logout():
-    return render_template("logout.html")
-
+    return render_template("login.html")
 
 @app.route('/ordered/')  # для подтверждения отправки
 def ordered():
